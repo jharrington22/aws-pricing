04c19e82fcc2642ec9e594d0da648d1c2b34c50a
shrutijain <shruti.j2007@gmail.com>
2019-06-28 16:33:10 -0400

    EBS Volumes Pricing List

--- ebs_pricing_list.py
+++ ebs_pricing_list.py
@@ -7,8 +7,8 @@ us_east_1_session = boto3.Session(region_name='us-east-1')
 session = boto3.Session(region_name='eu-west-2')
 
 pricing_client = us_east_1_session.client('pricing')
+resources = {}
 
-ec2 = session.client('ec2')
 
 region_short_names = {
     "ap-southeast-1": "Asia Pacific (Singapore)",
@@ -42,16 +42,6 @@ volume_types = {
     "standard": "Magnetic"
 }
 
-usage_type = {
-    "region": {
-        "us-east-1": {
-            "gp2": {
-                "UE"
-            }
-        }
-    }
-}
-
 resources = {}
 aws_region = list(region_short_names.keys())
 for region in aws_region:
@@ -59,28 +49,69 @@ for region in aws_region:
         "EBS" : {}
     }
 
-price_list = {}   
-while True:
-    pl = pricing_client.get_products(ServiceCode="AmazonEC2", 
-                                        Filters=[
-                                            {'Type': 'TERM_MATCH', 'Field': 'productFamily', 'Value': 'storage'}, 
-                                            {'Type': 'TERM_MATCH', 'Field': 'volumeType', 'Value': 'Provisioned IOPS'}
-                                            ])
-    if "PriceList" in price_list:
-            price_list["PriceList"] = price_list["PriceList"] + pl["PriceList"]
-    else:
-        price_list["PriceList"] = pl["PriceList"]
-    try:
-        NextTokenKey = pl["NextToken"]
-    except KeyError:
-        break
+price_list = {}  
+tokens = []
+token_count = 0
 
-for item in price_list["PriceList"]:
-    price_item = json.loads(item)
-    terms = price_item["terms"]
-    pprint.pprint(price_item)
-    if "OnDemand" in terms:
-        product_sku = list(terms["OnDemand"].keys())
-        pd = terms["OnDemand"][product_sku[0]]["priceDimensions"]
+# while True:
+#     pl = pricing_client.get_products(ServiceCode="AmazonEC2", 
+#                                         Filters=[
+#                                             {'Type': 'TERM_MATCH', 'Field': 'productFamily', 'Value': 'storage'}
+#                                             ])
+#     if "PriceList" in price_list:
+#         price_list["PriceList"] = price_list["PriceList"] + pl["PriceList"]
         
+#     else:
+#         price_list["PriceList"] = pl["PriceList"]
+#     try:
+#         NextTokenKey = pl["NextToken"]
+#         if not NextTokenKey in tokens:
+#             tokens.append(NextTokenKey)
+#             token_count += 1
+#             print(token_count)
+#         else:
+#             print("Token already seen? {}".format(NextTokenKey))
+#             break
+#     except KeyError:
+#         break
+
+paginator = pricing_client.get_paginator('get_products')
+resp_pages = paginator.paginate(ServiceCode="AmazonEC2", 
+                                        Filters=[
+                                            {'Type': 'TERM_MATCH', 'Field': 'productFamily', 'Value': 'storage'}
+                                            ])
+pprint.pprint(price_list)
+# with open('ebs_pricing_file.json', 'w') as ebs_file:
+#     ebs_file.write(json.dumps(price_list["PriceList"]))
+
+# pprint.pprint(price_list)
+for page in resp_pages:
+    for item in page["PriceList"]:
+        price_item = json.loads(item)
+        terms = price_item["terms"]
+        region = list(region_short_names.keys())[list(region_short_names.values()).index(price_item["product"]["attributes"]["location"])]
         
+        volume_type = list(volume_types.keys())[list(volume_types.values()).index(price_item["product"]["attributes"]["volumeType"])]
+        
+        if "OnDemand" in terms:
+            product_sku = list(terms["OnDemand"].keys())
+            pd = terms["OnDemand"][product_sku[0]]["priceDimensions"]
+            
+            product_price_sku = list(pd.keys())
+            price = pd[product_price_sku[0]]['pricePerUnit']["USD"]
+
+            description = pd[product_price_sku[0]]["description"]
+        
+            if not volume_type in resources[region]["EBS"]:
+                resources[region]["EBS"][volume_type] = {}
+            if not "OnDemand" in resources[region]["EBS"][volume_type]:
+                resources[region]["EBS"][volume_type]["OnDemand"] = {}
+
+            resources[region]["EBS"][volume_type]["OnDemand"] = {
+                            "Description": description,
+                            "UsageType": price_item["product"]["attributes"]["usagetype"],
+                            "Location": price_item["product"]["attributes"]["location"],
+                            "Max Volume Size": price_item["product"]["attributes"]["maxVolumeSize"],
+                            "USD": price
+                            } 
+pprint.pprint(resources)
