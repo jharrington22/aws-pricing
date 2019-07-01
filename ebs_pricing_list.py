2b946de78c09418c028b0f4027ac3251289df98f
shrutijain <shruti.j2007@gmail.com>
2019-07-01 15:35:22 -0400

    EBS pricing list

--- ebs_pricing_list.py
+++ ebs_pricing_list.py
@@ -76,11 +76,11 @@ token_count = 0
 #         break
 
 paginator = pricing_client.get_paginator('get_products')
-resp_pages = paginator.paginate(ServiceCode="AmazonEC2", 
-                                        Filters=[
-                                            {'Type': 'TERM_MATCH', 'Field': 'productFamily', 'Value': 'storage'}
-                                            ])
-pprint.pprint(price_list)
+resp_pages = paginator.paginate(ServiceCode="AmazonEC2")
+                                        # Filters=[
+                                        #     {'Type': 'TERM_MATCH', 'Field': 'productFamily', 'Value': 'storage'}
+                                        #     ])
+
 # with open('ebs_pricing_file.json', 'w') as ebs_file:
 #     ebs_file.write(json.dumps(price_list["PriceList"]))
 
@@ -89,29 +89,31 @@ for page in resp_pages:
     for item in page["PriceList"]:
         price_item = json.loads(item)
         terms = price_item["terms"]
-        region = list(region_short_names.keys())[list(region_short_names.values()).index(price_item["product"]["attributes"]["location"])]
         
-        volume_type = list(volume_types.keys())[list(volume_types.values()).index(price_item["product"]["attributes"]["volumeType"])]
         
-        if "OnDemand" in terms:
-            product_sku = list(terms["OnDemand"].keys())
-            pd = terms["OnDemand"][product_sku[0]]["priceDimensions"]
+        if "volumeType" in price_item["product"]["attributes"]:
+            volume_type = list(volume_types.keys())[list(volume_types.values()).index(price_item["product"]["attributes"]["volumeType"])]
+            region = list(region_short_names.keys())[list(region_short_names.values()).index(price_item["product"]["attributes"]["location"])]
             
-            product_price_sku = list(pd.keys())
-            price = pd[product_price_sku[0]]['pricePerUnit']["USD"]
+            if "OnDemand" in terms:
+                product_sku = list(terms["OnDemand"].keys())
+                pd = terms["OnDemand"][product_sku[0]]["priceDimensions"]
+                
+                product_price_sku = list(pd.keys())
+                price = pd[product_price_sku[0]]['pricePerUnit']["USD"]
 
-            description = pd[product_price_sku[0]]["description"]
-        
-            if not volume_type in resources[region]["EBS"]:
-                resources[region]["EBS"][volume_type] = {}
-            if not "OnDemand" in resources[region]["EBS"][volume_type]:
-                resources[region]["EBS"][volume_type]["OnDemand"] = {}
+                description = pd[product_price_sku[0]]["description"]
+            
+                if not volume_type in resources[region]["EBS"]:
+                    resources[region]["EBS"][volume_type] = {}
+                if not "OnDemand" in resources[region]["EBS"][volume_type]:
+                    resources[region]["EBS"][volume_type]["OnDemand"] = {}
 
-            resources[region]["EBS"][volume_type]["OnDemand"] = {
-                            "Description": description,
-                            "UsageType": price_item["product"]["attributes"]["usagetype"],
-                            "Location": price_item["product"]["attributes"]["location"],
-                            "Max Volume Size": price_item["product"]["attributes"]["maxVolumeSize"],
-                            "USD": price
-                            } 
-pprint.pprint(resources)
+                resources[region]["EBS"][volume_type]["OnDemand"] = {
+                                "Description": description,
+                                "UsageType": price_item["product"]["attributes"]["usagetype"],
+                                "Location": price_item["product"]["attributes"]["location"],
+                                "Max Volume Size": price_item["product"]["attributes"]["maxVolumeSize"],
+                                "USD": price
+                                } 
+pprint.pprint(resources["ca-central-1"])
