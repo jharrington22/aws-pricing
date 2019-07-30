3f5aa5c267a2d5987345c82cd1b0fe22cac0c3ac
shrutijain <shruti.j2007@gmail.com>
2019-07-30 10:05:47 -0400

    Setup configure file

--- setup.py
+++ setup.py
@@ -6,19 +6,26 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "aws_audit",
-    version = "0.0.1",
-    author = "Shruti Jain",
-    author_email = "shrjain@redhat.com",
-    description = ("A script to audit AWS resources and total spending"),
-    license = "Apache License 2.0",
-    keywords = "aws audit",
-    url = "https://github.com/openshift/aws-pricing/",
+    name="aws_audit",
+    version="0.0.1",
+    author="Shruti Jain",
+    author_email="shrjain@redhat.com",
+    description="A script to audit AWS ,resources and total spending",
+    license="Apache License 2.0",
+    keywords="aws audit",
+    url="https://github.com/openshift/aws-pricing/",
     packages=['aws_audit'],
     long_description=read('README.md'),
+    install_requires=[
+        'boto3>=1.9.163',
+        'prettytable>=0.7.2',
+        'argparse'
+        
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: AWS Pricing",
         "License :: OSI Approved :: Apache License 2.0",
+        "Programming Language :: Python :: 3.7.3"
     ],
 )
