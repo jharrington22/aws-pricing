44fb3b9abfd6dbf7bcd0ba7ee7acbb2cef381e0a
Shruti Jain <35978103+shrutijain18@users.noreply.github.com>
2019-07-30 10:21:42 -0400

    Delete connection.py

--- connection.py
+++
@@ -1,8 +0,0 @@
-import boto3 
-import sys
-
-#Connection to the API endpoints
-region = boto3.Session(region_name='us-east-1')
-session = boto3.Session(region_name='eu-west-2')
-ec2 = session.client('ec2')
-pricing_client = region.client('pricing')
