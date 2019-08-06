ec2348cbfd09b214620040a15f1ea8aa9789061c
Shruti Jain <35978103+shrutijain18@users.noreply.github.com>
2019-08-06 12:11:59 -0400

    Updated functions (#7)

--- aws_audit/constants.py
+++ aws_audit/constants.py
@@ -1,31 +1,28 @@
-
-#Region short names
+       
 region_short_names = {
-    "ap-southeast-1": "Asia Pacific (Singapore)",
-    "eu-central-1": "EU (Frankfurt)",
-    "eu-west-1": "EU (Ireland)",
-    "ap-northeast-1": "Asia Pacific (Tokyo)",
-    "ap-northeast-2": "Asia Pacific (Seoul)",
-    "ap-south-1": "Asia Pacific (Mumbai)",
-    "us-east-2": "US East (Ohio)",
-    "eu-north-1": "EU (Stockholm)",
-    "eu-west-3": "EU (Paris)",
-    "us-west-1": "US West (N. California)",
-    "ap-southeast-2": "Asia Pacific (Sydney)",
-    "us-east-1": "US East (N. Virginia)",
-    "eu-west-2": "EU (London)",
-    "sa-east-1": "South America (Sao Paulo)",
-    "us-west-2": "US West (Oregon)",
-    "ca-central-1": "Canada (Central)",
-    "ap-east-1": "Ap East",
-    "us-gov": "AWS GovCloud (US)",
-    "asia-pacific": "Asia Pacific (Hong Kong)",
-    "us-gov-east": "AWS GovCloud (US-East)",
-    "asia-pacific-ol": "Asia Pacific (Osaka-Local)"
+    "Asia Pacific (Singapore)": "ap-southeast-1",
+    "EU (Frankfurt)": "eu-central-1",
+    "EU (Ireland)": "eu-west-1",
+    "Asia Pacific (Tokyo)": "ap-northeast-1",
+    "Asia Pacific (Seoul)": "ap-northeast-2",
+    "Asia Pacific (Mumbai)": "ap-south-1",
+    "US East (Ohio)": "us-east-2",
+    "EU (Stockholm)": "eu-north-1",
+    "EU (Paris)": "eu-west-3",
+    "US West (N. California)": "us-west-1",
+    "Asia Pacific (Sydney)": "ap-southeast-2",
+    "US East (N. Virginia)": "us-east-1",
+    "EU (London)": "eu-west-2",
+    "South America (Sao Paulo)": "sa-east-1",
+    "US West (Oregon)": "us-west-2",
+    "Canada (Central)": "ca-central-1",
+    "Ap East": "ap-east-1",
+    "AWS GovCloud (US)": "us-gov",
+    "Asia Pacific (Hong Kong)": "asia-pacific",
+    "AWS GovCloud (US-East)": "us-gov-east",
+    "Asia Pacific (Osaka-Local)": "asia-pacific-ol",
+    "Middle East (Bahrain)": "middle-east"
 }
 
-#Dictionary to store values
-resources = {}
-
 #List of keys in the dict region_short_names
-aws_region = list(region_short_names.keys())
+aws_region = list(region_short_names.values())
